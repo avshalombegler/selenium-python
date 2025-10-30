87971cf56fb6ce82c8d5e1d87c5a9e9cd8add78e
Avshalom Begler <avshalom210@gmail.com>
2025-10-30 14:35:24 +0200

    feature/test dropdown list page

--- utils/locators.py
+++ utils/locators.py
@@ -26,7 +26,7 @@ class AbTestingPageLocators:
 
 
 class AddRemoveElementsPageLocators:
-    PAGE_LOADED_INDICATOR = (By.CSS_SELECTOR, "h3")
+    PAGE_LOADED_INDICATOR = (By.CSS_SELECTOR, "div#content h3")
     ADD_ELEMENT_BTN = (By.CSS_SELECTOR, ".example > button")
     DELETE_BTN = (By.CSS_SELECTOR, "#elements > button:first-child")
     DELETE_BTNS = (By.CSS_SELECTOR, "#elements > button")
